eb35750e3786144d16f01d921febfba587446abc
Jan romel <janjancaranguian@gmail.com>
2024-04-21 10:44:11 +0800

    Make the movement of blob

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,83 @@
 const canvas = document.querySelector('canvas')!;
-
 const c = canvas?.getContext('2d');
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-const x = 20
-const y = 20
-const radius = 10
-const starting = 0
-const ending = Math.PI * 2
-const isCounterClockwise = false
+let x = 100;
+let y = 100;
+let radius = 20;
+let starting = 0;
+let ending = Math.PI * 2;
+let isCounterClockwise = false;
+let dx = 0
+let dy = 0
 
+
+c?.clearRect(0, 0, window.innerWidth, window.innerHeight);
 c?.beginPath();
 c?.arc(x, y, radius, starting, ending, isCounterClockwise);
-c?.stroke()
+c?.stroke();
 
-window.addEventListener('keydown', (event) => {
-  movement(event.key)
-})
+function animate() {
+  requestAnimationFrame(animate);
+  createCircle();
 
-function movement(key : string): void {
-  switch(key) {
-    case 'ArrowUp' : 
-      console.log('ArrowUp');
+  x += dx
+  y += dy
+}
+
+animate();
+
+
+
+
+
+
+
+
+
+
+
+
+function createCircle() {
+  c?.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  c?.beginPath();
+  c?.arc(x, y, radius, starting, ending, isCounterClockwise);
+  c?.fill();
+  c?.stroke();
+}
+
+
+document.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowUp':
+      dy = -5; // Move up
+      dx = 0
       break;
-    case 'ArrowDown' :
-      console.log('ArrowDown');
+    case 'ArrowDown':
+      dy = 5; // Move down
+      dx = 0
       break;
-    case 'ArrowLeft' :
-      console.log('ArrowLeft');
+    case 'ArrowLeft':
+      dx = -5; // Move left
+      dy = 0
       break;
-    case 'ArrowRight' : 
-      console.log('ArrowRight');
+    case 'ArrowRight':
+      dx = 5; // Move right
+      dy = 0
       break;
   }
-}
+  console.log(`dx: ${dx} dy: ${dy}`)
+});
 
-function animate(): void {
 
-}
+
+
+
+
+
+
+
+
+
+
