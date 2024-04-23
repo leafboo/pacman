be00ac406f4a7b9e29f6c614b8eb1d92ed7db1d7
Jan romel <janjancaranguian@gmail.com>
2024-04-23 16:29:05 +0800

    Finish the collision detection for testing

--- src/test.ts
+++ src/test.ts
@@ -8,7 +8,7 @@ canvass.height = window.innerHeight
 
 let x = 400;
 let y = 50;
-let dx = 0;
+let dx = 0; 
 let dy = 0;
 
 const boxWidth = 20;
@@ -23,17 +23,12 @@ context.fillRect(x, y, 20, 20);
 context.fillStyle = 'blue'; // Set fill color to blue
 context.fillRect(50, 50, 20, 20);
 
-// Third square
-context.fillStyle = 'blue'; // Set fill color to blue
-context.fillRect(30, 100, 20, 20);
-
-
 
 // movements
 document.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'ArrowUp':
-      dy = -5; // Move up
+      dy = -5; // Move up 
       dx = 0;
       break;
 
@@ -62,26 +57,28 @@ function animate() {
   context.fillStyle = 'red'; // Set fill color to red
   context.fillRect(x, y, 20, 20);
 
-  // Second square
-  context.fillStyle = 'blue'; // Set fill color to blue
-  context.fillRect(900, 50, 20, 20);
-
-  // Third square
-  context.fillStyle = 'blue'; // Set fill color to blue
-  context.fillRect(100, 50, 20, 20);
 
   const box1Left = 900
-  const box1Right = 920
-  const box2Right = 100
+  const box1Top = 200;
 
-  if (x + boxWidth >= box1Left && x <= box1Left + boxWidth && y + 1 < 50 + boxWidth) {
-    dx = 0;
-    x -= 2;
+  // Second square
+  context.fillStyle = 'blue'; // Set fill color to blue
+  context.fillRect(box1Left, box1Top, 20, 20);
+
+  
+
+  function isColliding(): boolean {
+    if (x + boxWidth >= box1Left - (dx - 1) && x + (dx + 1)<= box1Left + boxWidth && y + (dy + 1) <= box1Top + boxWidth && y + boxWidth >= box1Top - (dy - 1)) {
+      return true;
+    }
+    return false
   }
-  if (x + boxWidth >= box2Right && x <= box2Right + boxWidth && y + 1 < 50 + boxWidth) {
+  
+  if (isColliding()) {
     dx = 0;
-    x += 2;
+    dy = 0;
   }
+ 
   
 
   x += dx;
